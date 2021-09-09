2a0dd33d46527f72b3c4cd9f18ae6cf9351d88aa
suhas5979 <suhassuryavanshi5979@gmail.com>
2021-09-10 00:06:47 +0530

    deployment setup

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports ={
-    MONGO_URI:"mongodb+srv://alumniPortal:alumniPortal%40123@cluster0.o0ly4.mongodb.net/alumniPortalDB?retryWrites=true&w=majority",
-    MONGO_URI_2:"mongodb://alumniPortal:alumniPortal%40123@cluster0-shard-00-00.o0ly4.mongodb.net:27017,cluster0-shard-00-01.o0ly4.mongodb.net:27017,cluster0-shard-00-02.o0ly4.mongodb.net:27017/alumniPortalDB?ssl=true&replicaSet=atlas-e31l59-shard-0&authSource=admin&retryWrites=true&w=majority",
-    SECRET_KEY:"MY_SECRET_KEY"
+if(process.env.NODE_ENV==='production'){
+    module.exports = require('./dev') ;
+}else{
+   module.exports = require('./dev') ;
 }
